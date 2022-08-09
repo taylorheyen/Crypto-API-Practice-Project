c6b55afaa8dd3e09dee237b98f67f7474bec7deb
Taylor Heyen <taylordheyen@gmail.com>
2022-08-08 22:20:11 -0700

    Add files via upload

---
+++ Crypto API Practice Project.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' 
+#Original Sandbox Environment: 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '9c8a4f59-d950-4337-8cc6-0777c81b8b7b',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  #print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+
+# In[2]:
+
+
+type(data)
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+pd.set_option('display.max_columns', None)
+
+
+# In[6]:
+
+
+pd.json_normalize(data['data'])
+
+
+# In[ ]:
+
+
+
+
